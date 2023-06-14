4c1a0c2b7864beae06e1c697c5e5ac70b1d13d60
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-06-15 00:37:33 +0200

    Add ESLint

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Hello world from fly.io</h1>
-    </main>
-  );
-}
+const HomePage = () => (
+  <main>
+    <h1>Hello world from fly.io</h1>
+  </main>
+);
+
+export default HomePage;
