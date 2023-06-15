0ed214c7355ea10c223ecb4fb61fff1df950b703
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-06-15 21:16:46 +0200

    SQLite wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-const HomePage = () => (
-	<main>
-		<h1>Hello world from fly.io</h1>
-	</main>
-);
+export const dynamic = "force-dynamic";
+
+const HomePage = () => {
+	console.log("DB_PATH", process.env["DATABASE_PATH"]);
+	console.log("REGION", process.env["FLY_REGION"]);
+
+	return (
+		<main>
+			<h1>Hello world from fly.io</h1>
+		</main>
+	);
+};
 
 export default HomePage;
---
+++ lib/db.ts
@@ -0,0 +1,7 @@
+// import SqliteDatabase from "better-sqlite3";
+// import { BetterSQLite3Database, drizzle } from "drizzle-orm/better-sqlite3";
+
+// const sqlite = new SqliteDatabase(process.env);
+// const db = drizzle(sqlite);
+
+export const db = null;
